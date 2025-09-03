e8e36a2bd4f88c9075ab7ebe5a622b9df9f14d2b
Sergey <dev@skuspekov.ru>
2025-09-03 18:42:36 +0300

    feat: add solution for L2.11

---
+++ L2/L2.11/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	input := []string{"пятак", "пятка", "тяпка", "листок", "слиток", "столик", "стол"}
+
+	m := make(map[string][]string)
+	annMap := make(map[string]string) // мапа чтобы хранить анограмму для каждого значения чтобы не сортировать ее постоянно
+
+	for _, i := range input {
+		currentAnogram := toSortedBytesSlice(i)
+
+		if hasAnogram, ok := annMap[currentAnogram]; !ok {
+			annMap[currentAnogram] = i
+		} else {
+			m[hasAnogram] = append(m[hasAnogram], i)
+		}
+
+	}
+
+	fmt.Printf("annMap: %v\n", len(annMap))
+	fmt.Printf("m: %v\n", m)
+
+}
+
+func toSortedBytesSlice(s string) string {
+	slice := []byte(s)
+
+	slices.Sort(slice)
+
+	return string(slice)
+
+}
